5dbda78922e0aa7fc2e56ac63cbfb2adca6a2cff
VictorXiao <843731025@qq.com>
2016-11-14 14:02:31 +0800

    Initial commit

---
+++ app/src/main/java/com/victor_xiao/WordBookApp/WordsApplication.java
@@ -0,0 +1,19 @@
+package com.victor_xiao.WordBookApp;
+
+import android.app.Application;
+import android.content.Context;
+
+
+public class WordsApplication extends Application{
+    private static Context context;
+    public static Context getContext(){
+        return WordsApplication.context;
+    }
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        WordsApplication.context=getApplicationContext();
+    }
+}
